07ed41855360fa12d0ac65a05b16ff7ca1d1d7dc
LAPTOP-Q8G0SFJB\jug78 <jug78@naver.com>
2021-12-18 16:10:35 +0900

    Sequelize 초기화

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-let db = {};
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
@@ -18,7 +18,7 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-7) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
@@ -31,7 +31,7 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
---
+++ test/market.js
@@ -0,0 +1,71 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+	const market = sequelize.define("Market", {
+		id: {
+			type: DataTypes.INTEGER,
+			unique: true,
+			allowNull: true,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		likenum: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		foodtag: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		local: {
+			type: DataTypes.JSON,
+			allowNull: true,
+		},
+		seatnum: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		M_1: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		M_2: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		M_3: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		M_4: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+		categ: {
+			type: DataTypes.JSON,
+			allowNull: false,
+		},
+		mood: {
+			type: DataTypes.JSON,
+			allowNull: false,
+		},
+		weather: {
+			type: DataTypes.JSON,
+			allowNull: true,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, {
+		underscored: true,
+		freezeTableName: true,
+		modelName: 'Market',
+		timestamps: false,
+		tableName: "market"
+	});
+	return market;
+};
