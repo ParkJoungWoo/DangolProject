60ffb00bb87f124113ffdbf5cab453cb4b939dbd
LAPTOP-Q8G0SFJB\jug78 <jug78@naver.com>
2021-12-18 16:17:01 +0900

    Sequelize 테스트

--- app.js
+++ app.js
@@ -2,39 +2,39 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-// const sequelize = require("./models").sequelize;
-// const model = require("./models");
+const sequelize = require("./models").sequelize;
+const model = require("./models");
 
-//sequelize.sync();
+sequelize.sync();
 
 let test = []
 app.use(express.static(`${__dirname}/src`));
 app.use(express.json());
 app.use(cors());
 
-// app.post("/upup", (req, res) => {
-// 	//const bodyObject = JSON.parse(req.body);
-// 	//console.log(bodyObject);
-// 	//console.log(req.body[0]);
-// 	console.log(req.body[0].mood);
+app.post("/upup", (req, res) => {
+	//const bodyObject = JSON.parse(req.body);
+	//console.log(bodyObject);
+	//console.log(req.body[0]);
+	console.log(req.body[0].mood);
 	
-// 	for (let i = 0; i < req.body.length; i++)
-// 	model.Market.create({
-// 	"id": req.body[i].id,
-// 	"name": req.body[i].name,
-// 	"foodtag": req.body[i].foodtag,
-// 	"likenum": req.body[i].likenum,
-// 	"local": req.body[i].local,
-// 	"seatnum": req.body[i].seatnum,
-// 	"M_1": req.body[i].M_1,
-// 	"M_2": req.body[i].M_2,
-// 	"M_3": req.body[i].M_3,
-// 	"M_4": req.body[i].M_4,
-// 	"categ": req.body[i].categ,
-// 	"mood": req.body[i].mood,
-// 	"weather": req.body[i].weather,
-// 	"image": req.body[i].image
-// 	});
+	for (let i = 0; i < req.body.length; i++)
+	model.Market.create({
+	"id": req.body[i].id,
+	"name": req.body[i].name,
+	"foodtag": req.body[i].foodtag,
+	"likenum": req.body[i].likenum,
+	"local": req.body[i].local,
+	"seatnum": req.body[i].seatnum,
+	"M_1": req.body[i].M_1,
+	"M_2": req.body[i].M_2,
+	"M_3": req.body[i].M_3,
+	"M_4": req.body[i].M_4,
+	"categ": req.body[i].categ,
+	"mood": req.body[i].mood,
+	"weather": req.body[i].weather,
+	"image": req.body[i].image
+	});
 	
 	
 // });
