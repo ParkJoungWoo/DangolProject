d799fd4eede5fe180196858210f4845dd86306ef
Ubuntu <ubuntu@ip-172-31-32-174.ap-northeast-2.compute.internal>
2021-12-25 08:46:22 +0000

    final3

--- bin/www.js
+++ bin/www.js
@@ -3,13 +3,13 @@
 const app = require("../app");
 const https = require('https');
 const options = require('../pem_config.js').options;
-const PORT = 8000;
-/*
+const PORT = 8080;
+
 https.createServer(options, app).listen(PORT, () => {
 	console.log("start https server");
 });
-*/
+/*
 app.listen(PORT, () => {
     console.log("Start Server...");
 });
-
+*/
